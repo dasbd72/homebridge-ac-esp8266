a0f1426a0b65c5048cdb1aaa58444e70f8b28398
dasbd72 <twbd723@gmail.com>
2023-10-24 23:52:43 +0800

    modified from oznu/homebridge-daikin-esp8266

---
+++ include/settings.h
@@ -0,0 +1,5 @@
+/* GPIO Settings */
+#define IR_PIN 4     // D2, GPIO4
+#define DHT_PIN 5    // D1, GPIO5
+#define DHT_TYPE 22  // DHT11 = 11, DHT22 = 22
+#define AC_MODE 2    // DAIKIN = 1, PANASONIC = 2
